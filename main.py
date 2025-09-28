3cb97d0dff3fa62a9f97f85ec800690b5a968f10
ZahidHasan321 <dev.zahidhasan@gmail.com>
2025-09-28 14:02:20 +0600

    Cors origin error fixed

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 from fastapi import FastAPI
 from routers import airtable
 from mangum import Mangum
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 handler = Mangum(app)
 
+origins = [
+    "http://localhost:5173", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # The list of allowed domains
+    allow_credentials=True,           # Allow cookies/authorization headers
+    allow_methods=["*"],              # Allow all standard methods (GET, POST, PUT, DELETE, OPTIONS)
+    allow_headers=["*"],              # Allow all headers (Authorization, Content-Type, etc.)
+)
 @app.get("/")
 def read_root():
     return {"Hello from Team Autolinium, we are grinding"}
